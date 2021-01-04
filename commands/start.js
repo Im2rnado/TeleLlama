e20ece795c87333159f5658adbbdd838237b32df
im2rnado <yassin.bedier@gmail.com>
2021-01-04 16:03:52 +0200

    sxlar fix

--- commands/start.js
+++ commands/start.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: "start",
 	execute(ctx) {
-		ctx.reply("Welcome to Carbide, thanks for coming! We will give you all the information that you need.\n\n*What is Carbide*?\nCarbide is a Telegram and Discord Bot that let's you login to your Fortnite account and access lots of things which can't be done with other bots and can only be done ingame!\n\n*How can I use it*?\nCarbide is free but there are some commands which are limited to Premium users only. You can purchase access from any of our admins:\n• @im2rnado - BTC\n• @sxlar_sells -_ CashApp\n• @dingus69 - PayTM \n• @ehdan69 CashApp, PayPal, PayTM", { parse_mode: "markdown" });
+		ctx.reply("Welcome to Carbide, thanks for coming! We will give you all the information that you need.\n\n*What is Carbide*?\nCarbide is a Telegram and Discord Bot that let's you login to your Fortnite account and access lots of things which can't be done with other bots and can only be done ingame!\n\n*How can I use it*?\nCarbide is free but there are some commands which are limited to Premium users only. You can purchase access from any of our admins:\n• @im2rnado - BTC\n• @dingus69 - PayTM \n• @ehdan69 CashApp, PayPal, PayTM", { parse_mode: "markdown" });
 	},
 };
