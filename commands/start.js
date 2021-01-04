435cfd2c38be658105b1f4515f8e1a8b3f98511d
im2rnado <yassin.bedier@gmail.com>
2021-01-04 12:06:43 +0200

    start.js

---
+++ commands/start.js
@@ -0,0 +1,6 @@
+module.exports = {
+	name: "start",
+	execute(ctx) {
+		ctx.reply("Welcome to Carbide, thanks for coming! We will give you all the information that you need.\n\n*What is Carbide*?\nCarbide is a Telegram and Discord Bot that let's you login to your Fortnite account and access lots of things which can't be done with other bots and can only be done ingame!\n\n*How can I use it*?\nCarbide is free but there are some commands which are limited to Premium users only. You can purchase Premium right now from @im2rnado");
+	},
+};
